3651e4f41e0453a0fe7dce44e29a27f98b97934c
ViPiven <vitalijjpiven777@gmail.com>
2021-06-15 13:42:36 +0300

    Some fixes

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
-!/usr/bin/env sh
+#!/usr/bin/env sh
 
 # остановить публикацию при ошибках
 set -e
@@ -10,13 +10,13 @@ npm run build
 cd dist
 
 # если вы публикуете на пользовательский домен
-echo 'https://github.com/ViPiven/CryptoApp' > test_crypto
+echo 'https://github.com/ViPiven/CryptoApp' > CryptoApp
 
 git init
 git add -A
 git commit -m 'deploy'
 
 # если вы публикуете по адресу https://<USERNAME>.github.io
-git push -f git@github.com:ViPiven/CryptoApp.git master:gh-pages
+git push -f git@github.com:ViPiven/CryptoApp.git develop:gh-pages
 
 cd -
